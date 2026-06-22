711a1bbf87cfdd2ffd2dbc0bf0d0f4b73c90034d
Garrett B Ringle <gbt1056@wade.wmich.edu>
2026-06-22 12:15:54 -0400

    Working dash component boilerplate with webpack sourcemap fix

---
+++ dash_rgl/dash_rgl/_imports_.py
@@ -0,0 +1,5 @@
+from .RGLLayout import RGLLayout
+
+__all__ = [
+    "RGLLayout"
+]
---
+++ dash_rgl/src/lib/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import RGLLayout from './components/RGLLayout.react';
+
+export {
+    RGLLayout
+};
